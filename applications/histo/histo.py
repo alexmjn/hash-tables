28aa3fa7cd5a16ee21792d51bd388810f64932cc
alexmjn <a.jenkneary@gmail.com>
2020-07-09 15:56:03 -0400

    histo function

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,29 @@
-# Your code here
+import re
 
+def word_count(s, cache):
+    """count words in a string and cache the counts"""
+    # format string
+    s = s.lower()
+    pattern = re.compile(r"[^a-zA-Z\'\s]")
+    s = re.sub(pattern, "", s)
+    s = s.split()
+
+    for word in s:
+        if word in cache:
+            cache[word] += 1
+        else:
+            cache[word] = 1
+
+cache = {}
+
+with open("applications/histo/robin.txt", "r") as robin:
+    for line in robin:
+        word_count(line, cache)
+
+# sort cache
+sorted_cache = list(cache.items())
+sorted_cache.sort(key=lambda x: x[1], reverse=True)
+
+if __name__ == "__main__":
+    for word, count in sorted_cache:
+        print(f"{word.ljust(20)}{'#'*count}")
